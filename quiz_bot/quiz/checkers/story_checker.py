2a8c74cac0e9ee76baef927b181d1b14a7fd8d6d
Vlad Mukhamatnurov <v.mukhamatnurov@tinkoff.ru>
2020-09-07 19:46:11 +0500

    Created story checker

--- quiz_bot/quiz/checkers/story_checker.py
+++ quiz_bot/quiz/checkers/story_checker.py
@@ -1,41 +1,80 @@
+import enum
 import logging
 import re
-from typing import Match, Optional, Set
+from copy import copy
+from typing import List, Match, Optional, Pattern, Sequence
 
 import telebot
-from quiz_bot.entity import CheckedResult, ContextChallenge, ContextParticipant, RegularChallengeInfo
+from quiz_bot.entity import CheckedResult, ContextChallenge, ContextParticipant, StoryChallengeInfo
+from quiz_bot.entity.objects import StoryItem
 from quiz_bot.quiz.checkers.base_checker import BaseResultChecker
 
 logger = logging.getLogger(__name__)
 
 
+class StoryPatternDelimiter(str, enum.Enum):
+    LEFT = "{"
+    RIGHT = "}"
+
+
+class StoryPattern(Pattern[str], enum.Enum):
+    USERNAME = re.compile(rf"({StoryPatternDelimiter.LEFT}username{StoryPatternDelimiter.RIGHT})+")
+
+
 class AnswerMatchingMixin:
     @staticmethod
-    def _prepare_for_matching(text: str) -> str:
-        return text.strip().lower()
+    def _prepare_for_matching(text: str) -> List[str]:
+        return [x for x in text.strip().lower().split("\n") if x]
 
     @classmethod
-    def _search(cls, answer: str, expectation: str) -> Optional[Match[str]]:
-        return re.search(rf"({cls._prepare_for_matching(expectation)})+", cls._prepare_for_matching(answer))
+    def _search(cls, answer: str, expectation: str, allow_pattern_parsing: bool = False) -> Optional[Match[str]]:
+        result = copy(answer)
+        if allow_pattern_parsing:
+            for pattern in list(StoryPattern):
+                result = pattern.value.sub(expectation, result)
+        return re.search(rf"({expectation})+", result, re.I)
+
+    @staticmethod
+    def _lines_number_equal(answer_lines: Sequence[str], expected_items: Sequence[StoryItem]) -> bool:
+        lines_len = len(answer_lines)
+        items_len = len(expected_items)
+        if lines_len != items_len:
+            logger.debug(
+                "Not equal number of user lines and expected items: %s against %s!", lines_len, items_len,
+            )
+            return False
+        return True
 
     @classmethod
-    def _match(cls, answer: str, expectations: Set[str]) -> bool:
-        return any(cls._search(answer, expectation) for expectation in expectations)
+    def _match(cls, answer: str, items: Sequence[StoryItem]) -> bool:
+        lines = cls._prepare_for_matching(answer)
+        if not cls._lines_number_equal(lines, items):
+            return False
+        for line_num, line_value in enumerate(lines):
+            if all(
+                (
+                    cls._search(line_value, items[line_num].step.value),
+                    cls._search(line_value, items[line_num].construction),
+                    cls._search(line_value, items[line_num].text),
+                )
+            ):
+                continue
+            return False
+        return True
 
 
-class StoryResultChecker(BaseResultChecker[RegularChallengeInfo], AnswerMatchingMixin):
+class StoryResultChecker(BaseResultChecker[StoryChallengeInfo], AnswerMatchingMixin):
     def check_answer(
         self,
         participant: ContextParticipant,
         data: ContextChallenge,
-        info: RegularChallengeInfo,
+        info: StoryChallengeInfo,
         message: telebot.types.Message,
     ) -> CheckedResult:
         current_result = self._result_storage.get_last_result(participant_id=participant.id)
-        expectations = info.get_answer_variants(current_result.phase)
-        if not self._match(answer=message.text, expectations=expectations):
+        if not self._match(answer=message.text, items=info.items):
             logger.debug(
-                "User '%s' given incorrect answer for phase %s, challenge %s",
+                "User '%s' given incorrect story for challenge %s",
                 participant.user.nick_name,
                 current_result.phase,
                 data.id,
@@ -43,9 +82,6 @@ class StoryResultChecker(BaseResultChecker[RegularChallengeInfo], AnswerMatching
             return CheckedResult(correct=False, finish_condition_reached=False, next_phase=current_result.phase)
 
         logger.info(
-            "User '%s' given CORRECT answer for phase %s, challenge %s",
-            participant.user.nick_name,
-            current_result.phase,
-            data.id,
+            "User '%s' given CORRECT story for challenge %s", participant.user.nick_name, current_result.phase, data.id,
         )
         return self._next_result(participant=participant, data=data, current_result=current_result,)
